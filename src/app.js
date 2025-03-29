f58e09367dcba81d7d35fbcd4c99a71c1bfb831e
Anabell <anabekk726@gmail.com>
2025-03-29 01:05:56 +0000

    p'

--- src/app.js
+++ src/app.js
@@ -9,20 +9,51 @@ window.onload = function() {
   //Domain name generator
   //we will be using multiple arrays
   //We want to output every possible permutation using each item in our arrays
+//1. Create a set of arrays to make our permutations
 
 let pronoun = ['the', 'our','your', 'my', 'her', 'his']; //outer loop
 let adjective = ['great', 'happy','genius', 'silly', 'thoughtful']; //inner loop
-let noun = ['cat', 'dog', 'llama', 'iguana', 'horse', 'cow', 'turtle'];
+let noun = ['cat', 'dog', 'llama', 'iguana', 'horse', 'cow', 'turtle', 'capcom', 'comcast'];
 let extension = ['.com', '.net', '.org', '.dev', '.us']
 
+//2. querySelect the  <ul> classname to add a list
 
+let listElements = document.querySelector('.domain')
+let lisOfItems = [];
 
 //Loop 
     for(let i=0; i < pronoun.length; ++i) {
       for(let j=0; j < adjective.length; ++j) {
         for(let n=0; n < noun.length; ++n) {
           for(let x=0; x < extension.length; ++x) {
-            console.log(pronoun[i] + adjective[j] + noun[n] + extension[x]);
+
+
+            let temp = document.createElement('li');
+            temp.innerHTML = `${pronoun[i]}${adjective[j]}${noun[n]}${extension[x]}`;
+            listElements.appendChild(temp);
+
+            // add a condition that checks for the last three chararcters
+            //if they are {"com"}, add a dot before it, capcom => cap.com
+            if (noun[n].endsWith('com')){
+
+              
+              //capcom => cap.com
+              result = .com
+
+              //make a new variable
+
+              d = result
+            
+              //append the firts part of the word
+              
+              //append a period
+              //append 'com'
+              //assign it to the console.log
+            
+
+              //
+            }           
+              console.log(pronoun[i] + adjective[j] + noun[n] + extension[x]);
             }
           }
         }
